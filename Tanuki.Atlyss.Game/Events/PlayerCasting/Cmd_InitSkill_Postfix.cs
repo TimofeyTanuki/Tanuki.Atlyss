a481a11c90b45b8a839f8d89af129b965a4215d2
Timofey Tanuki <timofey.tanuki@gmail.com>
2025-08-13 11:55:48 +0300

    Preview

---
+++ Tanuki.Atlyss.Game/Events/PlayerCasting/Cmd_InitSkill_Postfix.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace Tanuki.Atlyss.Game.Events.PlayerCasting;
+
+[HarmonyPatch(typeof(global::PlayerCasting), "Cmd_InitSkill", MethodType.Normal)]
+public static class Cmd_InitSkill_Postfix
+{
+    public delegate void EventHandler(global::PlayerCasting PlayerCasting);
+    public static event EventHandler OnInvoke;
+
+#pragma warning disable IDE0051
+    private static void Postfix(global::PlayerCasting __instance) =>
+        OnInvoke?.Invoke(__instance);
+#pragma warning restore IDE0051
+}
