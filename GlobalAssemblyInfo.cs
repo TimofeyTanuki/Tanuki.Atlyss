713a4c50cdd078ac269f22b57d73072eb4828a3f
Timofey Tanuki <timofey.tanuki@gmail.com>
2026-01-08 09:14:41 +0300

    Fix due to game update

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Tanuki")]
 [assembly: AssemblyCopyright("Copyright © Tanuki 2025")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.2.3")]
-[assembly: AssemblyFileVersion("2.2.3")]
+[assembly: AssemblyVersion("2.2.4")]
+[assembly: AssemblyFileVersion("2.2.4")]
