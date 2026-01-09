dfe16ed114c310d1c623c04eecc6dc377cd1b176
Timofey Tanuki <timofey.tanuki@gmail.com>
2026-01-09 15:40:00 +0300

    Patch update

--- Tanuki.Atlyss.Game/Patches/MapInstance/DeserializeSyncVars.cs
+++ Tanuki.Atlyss.Game/Patches/MapInstance/DeserializeSyncVars.cs
@@ -7,36 +7,51 @@ namespace Tanuki.Atlyss.Game.Patches.MapInstance;
 [HarmonyPatch(typeof(global::MapInstance), nameof(global::MapInstance.DeserializeSyncVars), MethodType.Normal)]
 public static class DeserializeSyncVars
 {
-    public delegate void EventHandler(global::MapInstance MapInstance, NetworkReader NetworkReader, bool InitialState);
-    public static event EventHandler OnPrefix;
-    public static event EventHandler OnPostfix;
-
-#pragma warning disable IDE0051
-    private static void Prefix(global::MapInstance __instance, NetworkReader reader, bool initialState, out int __state)
+    private struct State(int Position)
     {
-        __state = reader.Position;
-
-        Delegate[] Delegates = OnPrefix?.GetInvocationList();
-        if (Delegates is null)
-            return;
-
-        foreach (Delegate Delegate in OnPrefix.GetInvocationList())
-        {
-            ((EventHandler)Delegate)(__instance, reader, initialState);
-            reader.Position = __state;
-        }
+        public int Position = Position;
+        public bool Cancelled = false;
     }
 
-    private static void Postfix(global::MapInstance __instance, NetworkReader reader, bool initialState, int __state)
+    public delegate void Before(global::MapInstance MapInstance, NetworkReader NetworkReader, bool InitialState, ref bool ShouldAllow);
+    public static event Before OnBefore;
+
+    public delegate void After(global::MapInstance MapInstance, NetworkReader NetworkReader, bool InitialState, bool Cancelled);
+    public static event After OnAfter;
+
+#pragma warning disable IDE0051
+    private static bool Prefix(global::MapInstance __instance, NetworkReader reader, bool initialState, out State __state)
     {
-        Delegate[] Delegates = OnPostfix?.GetInvocationList();
+        int Position = reader.Position;
+        __state = new(Position);
+
+        Delegate[] Delegates = OnBefore?.GetInvocationList();
+        if (Delegates is null)
+            return true;
+
+        bool ShouldAllow = true;
+
+        foreach (Delegate Delegate in OnBefore.GetInvocationList())
+        {
+            ((Before)Delegate)(__instance, reader, initialState, ref ShouldAllow);
+            reader.Position = Position;
+        }
+
+        __state.Cancelled = !ShouldAllow;
+
+        return ShouldAllow;
+    }
+
+    private static void Postfix(global::MapInstance __instance, NetworkReader reader, bool initialState, State __state)
+    {
+        Delegate[] Delegates = OnAfter?.GetInvocationList();
         if (Delegates is null)
             return;
 
-        foreach (Delegate Delegate in OnPostfix.GetInvocationList())
+        foreach (Delegate Delegate in OnAfter.GetInvocationList())
         {
-            reader.Position = __state;
-            ((EventHandler)Delegate)(__instance, reader, initialState);
+            reader.Position = __state.Position;
+            ((After)Delegate)(__instance, reader, initialState, __state.Cancelled);
         }
     }
 #pragma warning restore IDE0051
