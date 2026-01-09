6995ef094eb8300a3d5adf84db3d92b156284aed
Timofey Tanuki <timofey.tanuki@gmail.com>
2026-01-09 21:03:47 +0300

    New accessor
    - GameWorldManager._currentDayNightCycleBuffer

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Tanuki")]
-[assembly: AssemblyCopyright("Copyright © Tanuki 2025")]
+[assembly: AssemblyCopyright("Copyright © Tanuki 2026")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.3.0")]
-[assembly: AssemblyFileVersion("2.3.0")]
+[assembly: AssemblyVersion("2.3.1")]
+[assembly: AssemblyFileVersion("2.3.1")]
---
+++ Tanuki.Atlyss.Game/Accessors/GameWorldManager.cs
@@ -0,0 +1,11 @@
+﻿using HarmonyLib;
+
+namespace Tanuki.Atlyss.Game.Accessors;
+
+public static class GameWorldManager
+{
+    public static readonly AccessTools.FieldRef<global::GameWorldManager, float> _currentDayNightCycleBuffer;
+
+    static GameWorldManager() =>
+        _currentDayNightCycleBuffer = AccessTools.FieldRefAccess<global::GameWorldManager, float>("_currentDayNightCycleBuffer");
+}
