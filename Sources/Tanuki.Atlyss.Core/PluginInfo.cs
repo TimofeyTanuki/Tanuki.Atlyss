41aff0aa434962a3d003e6b1ee95834b1bcc45bf
Timofey Tanuki <timofey.tanuki@gmail.com>
2026-02-22 07:56:12 +0300

    Duplicate version declaration fix

--- Sources/Tanuki.Atlyss.Core/PluginInfo.cs
+++ Sources/Tanuki.Atlyss.Core/PluginInfo.cs
@@ -5,5 +5,5 @@ public static class PluginInfo
     public const string
         NAME = "Tanuki.Atlyss",
         GUID = "9c00d52e10b8413f9ee4bfde81762442",
-        VERSION = "3.0.4";
+        VERSION = "3.0.0";
 }
