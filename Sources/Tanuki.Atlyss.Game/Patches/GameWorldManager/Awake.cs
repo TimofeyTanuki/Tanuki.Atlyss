62a2518f2029ddc54f1cb3bd0400b13dc7bdcff1
Timofey Tanuki <timofey.tanuki@gmail.com>
2026-07-20 08:04:18 +0300

    Create Awake.cs

---
+++ Sources/Tanuki.Atlyss.Game/Patches/GameWorldManager/Awake.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Tanuki.Atlyss.Game.Patches.GameWorldManager;
+
+[HarmonyPatch(typeof(global::GameWorldManager), "Awake", MethodType.Normal)]
+public sealed class Awake
+{
+    private static Action<global::GameWorldManager>? onPostfix;
+
+    public static event Action<global::GameWorldManager> OnPostfix
+    {
+        add
+        {
+            if (Utilities.Patches.EnsurePatched<Awake>())
+                onPostfix += value;
+        }
+        remove => onPostfix -= value;
+    }
+
+    [HarmonyPostfix, SuppressMessage("CodeQuality", "IDE0051")]
+    private static void Postfix(global::GameWorldManager __instance)
+    {
+        if (onPostfix is null)
+            return;
+
+        onPostfix.Invoke(__instance);
+    }
+}
